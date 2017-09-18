6a0ea58212d875b480c49f6eddba42c4cf8e5b6d
zim <rulea@list.ru>
2017-09-18 22:14:13 +0300

    change max to buy and mix to sell

--- cursmd/cursmd/spiders/cursmd_spider.py
+++ cursmd/cursmd/spiders/cursmd_spider.py
@@ -17,35 +17,35 @@ class CursMdSpider(Spider):
 
         final_list_info = [['Date', ymd, hms]]
 
-        max_usd_list = []
-        min_usd_list = []
-        max_eur_list = []
-        min_eur_list = []
-        max_rub_list = []
-        min_rub_list = []
-        max_ron_list = []
-        min_ron_list = []
-        max_uah_list = []
-        min_uah_list = []
-        max_gbp_list = []
-        min_gbp_list = []
-        max_chf_list = []
-        min_chf_list = []
+        buy_usd_list = []
+        sell_usd_list = []
+        buy_eur_list = []
+        sell_eur_list = []
+        buy_rub_list = []
+        sell_rub_list = []
+        buy_ron_list = []
+        sell_ron_list = []
+        buy_uah_list = []
+        sell_uah_list = []
+        buy_gbp_list = []
+        sell_gbp_list = []
+        buy_chf_list = []
+        sell_chf_list = []
 
-        max_usd_info = ['Buy usd']
-        min_usd_info = ['Sell usd']
-        max_eur_info = ['Buy eur']
-        min_eur_info = ['Sell eur']
-        max_rub_info = ['Buy rub']
-        min_rub_info = ['Sell rub']
-        max_ron_info = ['Buy ron']
-        min_ron_info = ['Sell ron']
-        max_uah_info = ['Buy uah']
-        min_uah_info = ['Sell uah']
-        max_gbp_info = ['Buy gbp']
-        min_gbp_info = ['Sell gbp']
-        max_chf_info = ['Buy chf']
-        min_chf_info = ['Sell chf']
+        buy_usd_info = ['Buy usd']
+        sell_usd_info = ['Sell usd']
+        buy_eur_info = ['Buy eur']
+        sell_eur_info = ['Sell eur']
+        buy_rub_info = ['Buy rub']
+        sell_rub_info = ['Sell rub']
+        buy_ron_info = ['Buy ron']
+        sell_ron_info = ['Sell ron']
+        buy_uah_info = ['Buy uah']
+        sell_uah_info = ['Sell uah']
+        buy_gbp_info = ['Buy gbp']
+        sell_gbp_info = ['Sell gbp']
+        buy_chf_info = ['Buy chf']
+        sell_chf_info = ['Sell chf']
 
 
         all_data = []
@@ -119,47 +119,31 @@ class CursMdSpider(Spider):
                 # Add all money to his list
                 # Buy money to buy list
                 # Sell money to sell list
-                if buy_usd and buy_usd != '-':
-                    max_usd_list.append(buy_usd)
+                def add_money_to_list(money, money_to_add):
+                    if money and money != '-':
+                        money_to_add.append(money)
 
-                if sell_usd and sell_usd != '-':
-                    min_usd_list.append(sell_usd)
-
-                if buy_eur and buy_eur != '-':
-                    max_eur_list.append(buy_eur)
-
-                if sell_eur and sell_eur != '-':
-                    min_eur_list.append(sell_eur)
-
-                if buy_rub and buy_rub != '-':
-                    max_rub_list.append(buy_rub)
-
-                if sell_rub and sell_rub != '-':
-                    min_rub_list.append(sell_rub)
-
-                if buy_ron and buy_ron != '-':
-                    max_ron_list.append(buy_ron)
-
-                if sell_ron and sell_ron != '-':
-                    min_ron_list.append(sell_ron)
-
-                if buy_uah and buy_uah != '-':
-                    max_uah_list.append(buy_uah)
-
-                if sell_uah and sell_uah != '-':
-                    min_uah_list.append(sell_uah)
-
-                if buy_gbp and buy_gbp != '-':
-                    max_gbp_list.append(buy_gbp)
-
-                if sell_gbp and sell_gbp != '-':
-                    min_gbp_list.append(sell_gbp)
-
-                if buy_chf and buy_chf != '-':
-                    max_chf_list.append(buy_chf)
-
-                if sell_chf and sell_chf != '-':
-                    min_chf_list.append(sell_chf)
+                # USD
+                add_money_to_list(buy_usd, buy_usd_list)
+                add_money_to_list(sell_usd, sell_usd_list)
+                # EUR
+                add_money_to_list(buy_eur, buy_eur_list)
+                add_money_to_list(sell_eur, sell_eur_list)
+                # RUB
+                add_money_to_list(buy_rub, buy_rub_list)
+                add_money_to_list(sell_rub, sell_rub_list)
+                # RON
+                add_money_to_list(buy_ron, buy_ron_list)
+                add_money_to_list(sell_ron, sell_ron_list)
+                # UAJ
+                add_money_to_list(buy_uah, buy_uah_list)
+                add_money_to_list(sell_uah, sell_uah_list)
+                # GBP
+                add_money_to_list(buy_gbp, buy_gbp_list)
+                add_money_to_list(sell_gbp, sell_gbp_list)
+                # CHF
+                add_money_to_list(buy_chf, buy_chf_list)
+                add_money_to_list(sell_chf, sell_chf_list)
 
                 if not bank_name:
                     break_while = 'break'
@@ -186,100 +170,100 @@ class CursMdSpider(Spider):
         # With names of banks
         # And change , to .
         for bank_data in all_data:
-            add_to_list(max_usd_list, bank_data, 3, max_usd_info)
+            add_to_list(buy_usd_list, bank_data, 3, buy_usd_info)
 
-            add_to_list(min_usd_list, bank_data, 4, min_usd_info)
+            add_to_list(sell_usd_list, bank_data, 4, sell_usd_info)
 
-            add_to_list(max_eur_list, bank_data, 5, max_eur_info)
+            add_to_list(buy_eur_list, bank_data, 5, buy_eur_info)
 
-            add_to_list(min_eur_list, bank_data, 6, min_eur_info)
+            add_to_list(sell_eur_list, bank_data, 6, sell_eur_info)
 
-            add_to_list(max_rub_list, bank_data, 7, max_rub_info)
+            add_to_list(buy_rub_list, bank_data, 7, buy_rub_info)
 
-            add_to_list(min_rub_list, bank_data, 8, min_rub_info)
+            add_to_list(sell_rub_list, bank_data, 8, sell_rub_info)
 
-            add_to_list(max_ron_list, bank_data, 9, max_ron_info)
+            add_to_list(buy_ron_list, bank_data, 9, buy_ron_info)
 
-            add_to_list(min_ron_list, bank_data, 10, min_ron_info)
+            add_to_list(sell_ron_list, bank_data, 10, sell_ron_info)
 
-            add_to_list(max_uah_list, bank_data, 11, max_uah_info)
+            add_to_list(buy_uah_list, bank_data, 11, buy_uah_info)
 
-            add_to_list(min_uah_list, bank_data, 12, min_uah_info)
+            add_to_list(sell_uah_list, bank_data, 12, sell_uah_info)
 
-            add_to_list(max_gbp_list, bank_data, 13, max_gbp_info)
+            add_to_list(buy_gbp_list, bank_data, 13, buy_gbp_info)
 
-            add_to_list(min_gbp_list, bank_data, 14, min_gbp_info)
+            add_to_list(sell_gbp_list, bank_data, 14, sell_gbp_info)
 
-            add_to_list(max_chf_list, bank_data, 15, max_chf_info)
+            add_to_list(buy_chf_list, bank_data, 15, buy_chf_info)
 
-            add_to_list(min_chf_list, bank_data, 16, min_chf_info)
+            add_to_list(sell_chf_list, bank_data, 16, sell_chf_info)
 
 
-        # Get only one maximum value if in list is many values
-        def get_one_max_num(money_value):
+        # Get only one maximum value from buy list if in list is many values
+        def get_one_buy_num(money_value):
             for money in money_value:
                 if isinstance(money, float):
                     return money
 
-        # Get only one minumum value if in list is many values
-        def get_one_min_num(money_value):
+        # Get only one minumum value from sell list if in list is many values
+        def get_one_sell_num(money_value):
             for money in money_value:
                 if isinstance(money, float):
                     return money
 
         # value - value
         # Ex 2 - 1
-        usd_result_total = get_one_max_num(max_usd_info) - get_one_min_num(min_usd_info)
-        eur_result_total = get_one_max_num(max_eur_info) - get_one_min_num(min_eur_info)
-        rub_result_total = get_one_max_num(max_rub_info) - get_one_min_num(min_rub_info)
-        ron_result_total = get_one_max_num(max_ron_info) - get_one_min_num(min_ron_info)
-        uah_result_total = get_one_max_num(max_uah_info) - get_one_min_num(min_uah_info)
-        gbp_result_total = get_one_max_num(max_gbp_info) - get_one_min_num(min_gbp_info)
-        chf_result_total = get_one_max_num(max_chf_info) - get_one_min_num(min_chf_info)
+        usd_result_total = get_one_buy_num(buy_usd_info) - get_one_sell_num(sell_usd_info)
+        eur_result_total = get_one_buy_num(buy_eur_info) - get_one_sell_num(sell_eur_info)
+        rub_result_total = get_one_buy_num(buy_rub_info) - get_one_sell_num(sell_rub_info)
+        ron_result_total = get_one_buy_num(buy_ron_info) - get_one_sell_num(sell_ron_info)
+        uah_result_total = get_one_buy_num(buy_uah_info) - get_one_sell_num(sell_uah_info)
+        gbp_result_total = get_one_buy_num(buy_gbp_info) - get_one_sell_num(sell_gbp_info)
+        chf_result_total = get_one_buy_num(buy_chf_info) - get_one_sell_num(sell_chf_info)
 
 
         # Print only for me
         def print_for_me(value, total, buy, sell):
             print(value + " %.2f" % total,
-                    '|', 'B -', get_one_max_num(buy),
-                    '|', 'S -', get_one_min_num(sell))
+                    '|', 'B -', get_one_buy_num(buy),
+                    '|', 'S -', get_one_sell_num(sell))
 
-        print_for_me("USD", usd_result_total, max_usd_info, min_usd_info)
-        print_for_me("EUR", eur_result_total, max_eur_info, min_eur_info)
-        print_for_me("RUB", rub_result_total, max_rub_info, min_rub_info)
-        print_for_me("RON", ron_result_total, max_ron_info, min_ron_info)
-        print_for_me("UAH", uah_result_total, max_uah_info, min_uah_info)
-        print_for_me("GBP", gbp_result_total, max_gbp_info, min_gbp_info)
-        print_for_me("CHF", chf_result_total, max_chf_info, min_chf_info)
+        print_for_me("USD", usd_result_total, buy_usd_info, sell_usd_info)
+        print_for_me("EUR", eur_result_total, buy_eur_info, sell_eur_info)
+        print_for_me("RUB", rub_result_total, buy_rub_info, sell_rub_info)
+        print_for_me("RON", ron_result_total, buy_ron_info, sell_ron_info)
+        print_for_me("UAH", uah_result_total, buy_uah_info, sell_uah_info)
+        print_for_me("GBP", gbp_result_total, buy_gbp_info, sell_gbp_info)
+        print_for_me("CHF", chf_result_total, buy_chf_info, sell_chf_info)
 
         # If value is > 0 we dont add it to csv
         if usd_result_total > 0:
-            final_list_info.append(max_usd_info)
-            final_list_info.append(min_usd_info)
+            final_list_info.append(buy_usd_info)
+            final_list_info.append(sell_usd_info)
             final_list_info.append(['Usd Total', "%.2f" % usd_result_total])
         elif eur_result_total > 0:
-            final_list_info.append(max_eur_info)
-            final_list_info.append(min_eur_info)
+            final_list_info.append(buy_eur_info)
+            final_list_info.append(sell_eur_info)
             final_list_info.append(['Eur Total', "%.2f" % eur_result_total])
         elif rub_result_total > 0:
-            final_list_info.append(max_rub_info)
-            final_list_info.append(min_rub_info)
+            final_list_info.append(buy_rub_info)
+            final_list_info.append(sell_rub_info)
             final_list_info.append(['Rub Total', "%.2f" % rub_result_total])
         elif ron_result_total > 0:
-            final_list_info.append(max_ron_info)
-            final_list_info.append(min_ron_info)
+            final_list_info.append(buy_ron_info)
+            final_list_info.append(sell_ron_info)
             final_list_info.append(['Ron Total', "%.2f" % ron_result_total])
         elif uah_result_total > 0:
-            final_list_info.append(max_uah_info)
-            final_list_info.append(min_uah_info)
+            final_list_info.append(buy_uah_info)
+            final_list_info.append(sell_uah_info)
             final_list_info.append(['Uah Total', "%.2f" % uah_result_total])
         elif gbp_result_total > 0:
-            final_list_info.append(max_gbp_info)
-            final_list_info.append(min_gbp_info)
+            final_list_info.append(buy_gbp_info)
+            final_list_info.append(sell_gbp_info)
             final_list_info.append(['Gbp Total', "%.2f" % gbp_result_total])
         elif chf_result_total > 0:
-            final_list_info.append(max_chf_info)
-            final_list_info.append(min_chf_info)
+            final_list_info.append(buy_chf_info)
+            final_list_info.append(sell_chf_info)
             final_list_info.append(['Chf Total', "%.2f" % chf_result_total])
         else:
             final_list_info.append(['Nothing today'])
